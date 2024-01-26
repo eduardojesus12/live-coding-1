4bd85962edd1a6bbee7d00f27b6b4904b8f6c4a4
aldobeltran <150940982+aldobeltran@users.noreply.github.com>
2024-01-25 23:39:19 -0600

    Ejercicio 1

---
+++ live-coding-1/app.js
@@ -0,0 +1,30 @@
+
+let miArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio', 'papel','cacahuates','pistaches','chicharon']
+const bocina = "bocina";
+
+const resultado = [];
+
+function bigWords (palabra, miArraypalabras){
+
+    resultado.push(palabra);
+    console.log(palabra);
+    miArraypalabras.forEach(elemento => {
+        if(palabra.length < elemento.length){
+            resultado.push(elemento);
+        }
+    });
+    console.log(resultado.toString());
+}
+    bigWords(bocina, miArray);
+
+    const contenedor = document.querySelector(".contenedor");
+
+    const imprimeLista =(string) =>{
+        string.forEach(valor =>{
+            const lista = document.createElement("li");
+            lista.innerText = valor;
+            contenedor.appendChild(lista);
+        })
+    }
+imprimeLista(resultado)
+//console.log(bigWord(string, myArray));
